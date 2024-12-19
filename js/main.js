94cf5139728737495f9067b156f74ae015ec381f
jdevis <josephine.roubertie@gmail.com>
2024-12-19 15:52:54 +0100

    begin display filters

--- js/main.js
+++ js/main.js
@@ -2,89 +2,16 @@
 import { recipes } from "../data/recipes.js";
 
 /** display recipes */
-class RecipeIngredients {
-	constructor(ingredients) {
-		this._ingredients = ingredients;
-	}
-	ingredientsCard() {
-		const ingredientsContainer = document.createElement("div");
-		ingredientsContainer.classList.add("col-6");
-		const ingredientsTemplate = `<p class="card-text small">
-										${this._ingredients.ingredient}
-										<small
-											class="text-body-secondary d-block"
-											>${this._ingredients.quantity ?? ""} ${this._ingredients.unit ?? ""}</small
-										>
-									</p>`;
-		ingredientsContainer.innerHTML = ingredientsTemplate;
-		return ingredientsContainer;
-	}
-}
-
-class RecipesCard {
-	constructor(recipes) {
-		this._recipes = recipes;
-	}
-	recipeCard() {
-		const recipeContainer = document.createElement("article");
-		recipeContainer.classList.add("col-4");
-		recipeContainer.setAttribute("id", this._recipes.id);
-		const recipeTemplate = `
-					<div class="card mb-3 position-relative">
-						<img
-							src="/assets/photos/${this._recipes.image}"
-							class="card-img-top"
-							alt=""
-						/>
-						<span
-							class="w-25 position-absolute top-25 end-25 rounded-pill text-center bg-warning"
-							>${this._recipes.time}min</span
-						>
-						<div class="card-body">
-							<h5 class="card-title">${this._recipes.name}</h5>
-							<p class="text-body-secondary text-uppercase">
-								Recette
-							</p>
-							<p class="card-text">
-								${this._recipes.description}
-							</p>
-							<div class="row">
-								<p class="text-body-secondary text-uppercase">
-									Ingrédients
-								</p>
-								<div class="row" id="ingredientsContainer-${this._recipes.id}"></div>	
-							</div>
-						</div>
-					</div>`;
-		recipeContainer.innerHTML = recipeTemplate;
-		return recipeContainer;
-	}
-}
-
-function displayRecipes() {
-	const cardWrapper = document.getElementById("recipeCards");
-	recipes.forEach((recipe) => {
-		const recipesTemplate = new RecipesCard(recipe);
-		cardWrapper.appendChild(recipesTemplate.recipeCard());
-		recipe.ingredients.forEach((ingredient) => {
-			const ingredientsWrapper = document.getElementById(
-				"ingredientsContainer-" + recipe.id
-			);
-			const ingredientTemplate = new RecipeIngredients(ingredient);
-			ingredientsWrapper.appendChild(
-				ingredientTemplate.ingredientsCard()
-			);
-		});
-		return recipesTemplate;
-	});
-}
-
 const cardTemplate = document.querySelector("[data-recipe-template]");
 const cardsWrapper = document.getElementById("recipeCards");
 const ingredientsTemplate = document.querySelector(
 	"[data-ingredients-template]"
 );
 let recipesArray = [];
+let ingredientsArray = [];
+let ingredientsFilterArray = [];
+let applianceArray = [];
+let ustensilsArray = [];
 
 recipesArray = recipes.map((recipe) => {
 	const card = cardTemplate.content.cloneNode(true).children[0];
@@ -98,7 +25,7 @@ recipesArray = recipes.map((recipe) => {
 	const ingredientsContainer = card.querySelector(
 		"[data-ingredients-container]"
 	);
-	recipe.ingredients.forEach((ing) => {
+	ingredientsArray = recipe.ingredients.map((ing) => {
 		const ingredientCard = ingredientsTemplate.content.cloneNode(true)
 			.children[0];
 		const ingredientTitle = ingredientCard.querySelector(
@@ -113,15 +40,30 @@ recipesArray = recipes.map((recipe) => {
 		ingredientTitle.textContent = ing.ingredient;
 		ingredientQuantity.textContent = ing.quantity;
 		ingredientUnit.textContent = ing.unit;
+		if (!ingredientsFilterArray.includes(ing.ingredient)) {
+			ingredientsFilterArray.push(ing.ingredient);
+		}
 		ingredientsContainer.append(ingredientCard);
+		return { ingredientTitle: ing.ingredient };
 	});
+	recipe.ustensils.forEach((element) => {
+		if (!ustensilsArray.includes(element)) {
+			ustensilsArray.push(element);
+		}
+	});
+	if (!applianceArray.includes(recipe.appliance)) {
+		applianceArray.push(recipe.appliance);
+	}
 	cardsWrapper.append(card);
 	return {
 		title: recipe.name,
 		description: recipe.description,
+		appliance: recipe.appliance,
 		element: card,
 	};
 });
+console.log(ingredientsFilterArray);
+/** display filters */
 
 /** searchBar */
 const searchInput = document.getElementById("search");
