20c7c093b74da9f46cb284d9cba5e4ba64af0a03
jdevis <josephine.roubertie@gmail.com>
2024-12-19 12:03:42 +0100

    add listener to search input

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Algo 2
 import { recipes } from "../data/recipes.js";
 
+/** display recipes */
 class RecipeIngredients {
 	constructor(ingredients) {
 		this._ingredients = ingredients;
@@ -8,10 +9,10 @@ class RecipeIngredients {
 	ingredientsCard() {
 		const ingredientsContainer = document.createElement("div");
 		ingredientsContainer.classList.add("col-6");
-		const ingredientsTemplate = `<p class="card-text">
+		const ingredientsTemplate = `<p class="card-text small">
 										${this._ingredients.ingredient}
 										<small
-											class="text-body-secondary"
+											class="text-body-secondary d-block"
 											>${this._ingredients.quantity ?? ""} ${this._ingredients.unit ?? ""}</small
 										>
 									</p>`;
@@ -27,6 +28,7 @@ class RecipesCard {
 	recipeCard() {
 		const recipeContainer = document.createElement("article");
 		recipeContainer.classList.add("col-4");
+		recipeContainer.setAttribute("id", this._recipes.id);
 		const recipeTemplate = `
 					<div class="card mb-3 position-relative">
 						<img
@@ -73,7 +75,73 @@ function displayRecipes() {
 				ingredientTemplate.ingredientsCard()
 			);
 		});
+		return recipesTemplate;
 	});
 }
 
-displayRecipes();
+const cardTemplate = document.querySelector("[data-recipe-template]");
+const cardsWrapper = document.getElementById("recipeCards");
+const ingredientsTemplate = document.querySelector(
+	"[data-ingredients-template]"
+);
+let recipesArray = [];
+
+recipesArray = recipes.map((recipe) => {
+	const card = cardTemplate.content.cloneNode(true).children[0];
+	const img = card.querySelector("[data-recipe-img]");
+	const recipeTitle = card.querySelector("[data-recipe-title]");
+	const desc = card.querySelector("[data-recipe-desc]");
+	const src = "/assets/photos/" + recipe.image;
+	img.setAttribute("src", src);
+	recipeTitle.textContent = recipe.name;
+	desc.textContent = recipe.description;
+	const ingredientsContainer = card.querySelector(
+		"[data-ingredients-container]"
+	);
+	recipe.ingredients.forEach((ing) => {
+		const ingredientCard = ingredientsTemplate.content.cloneNode(true)
+			.children[0];
+		const ingredientTitle = ingredientCard.querySelector(
+			"[data-ingredient-title]"
+		);
+		const ingredientQuantity = ingredientCard.querySelector(
+			"[data-ingredient-qty]"
+		);
+		const ingredientUnit = ingredientCard.querySelector(
+			"[data-ingredient-unit]"
+		);
+		ingredientTitle.textContent = ing.ingredient;
+		ingredientQuantity.textContent = ing.quantity;
+		ingredientUnit.textContent = ing.unit;
+		ingredientsContainer.append(ingredientCard);
+	});
+	cardsWrapper.append(card);
+	return {
+		title: recipe.name,
+		description: recipe.description,
+		element: card,
+	};
+});
+
+/** searchBar */
+const searchInput = document.getElementById("search");
+const message = document.querySelector("[data-message]");
+searchInput.addEventListener("input", (e) => {
+	const value = e.target.value;
+	let messageVisible = false;
+	recipesArray.forEach((recipe) => {
+		if (value.length > 2) {
+			const isVisible =
+				recipe.title.toLowerCase().includes(value) ||
+				recipe.description.toLowerCase().includes(value);
+			recipe.element.classList.toggle("d-none", !isVisible);
+			message.classList.toggle("d-none", messageVisible);
+		} else {
+			recipe.element.classList.remove("d-none");
+			message.classList.add("d-none");
+		}
+	});
+});
+
+/** init */
+//displayRecipes();
