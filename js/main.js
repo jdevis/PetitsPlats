61342e5d9cbe6cc97d6dc296eae76bc7486a27ff
jdevis <josephine.roubertie@gmail.com>
2024-12-20 11:29:27 +0100

    display filters and number of recipes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Algo 2
+/** Algo 2 */
 import { recipes } from "../data/recipes.js";
 
 /** display recipes */
@@ -40,17 +40,20 @@ recipesArray = recipes.map((recipe) => {
 		ingredientTitle.textContent = ing.ingredient;
 		ingredientQuantity.textContent = ing.quantity;
 		ingredientUnit.textContent = ing.unit;
+		ingredientsContainer.append(ingredientCard);
+		//store ingredients for filter
 		if (!ingredientsFilterArray.includes(ing.ingredient)) {
 			ingredientsFilterArray.push(ing.ingredient);
 		}
-		ingredientsContainer.append(ingredientCard);
 		return { ingredientTitle: ing.ingredient };
 	});
+	//store ustensils for filter
 	recipe.ustensils.forEach((element) => {
 		if (!ustensilsArray.includes(element)) {
 			ustensilsArray.push(element);
 		}
 	});
+	//store appliances for filter
 	if (!applianceArray.includes(recipe.appliance)) {
 		applianceArray.push(recipe.appliance);
 	}
@@ -62,28 +65,62 @@ recipesArray = recipes.map((recipe) => {
 		element: card,
 	};
 });
-console.log(ingredientsFilterArray);
+
 /** display filters */
+const ingredientsList = document.querySelector("[data-filter-ingredients]");
+const appliancesList = document.querySelector("[data-filter-appliances]");
+const ustensilsList = document.querySelector("[data-filter-ustensils]");
+const listTemplate = document.querySelector("[data-filter-list]");
+// ingredients filter
+ingredientsFilterArray.forEach((element) => {
+	const list = listTemplate.content.cloneNode(true).children[0];
+	const link = list.querySelector("[data-filter-link]");
+	link.textContent = element;
+	ingredientsList.children[0].append(list);
+});
+// appliances filter
+applianceArray.forEach((element) => {
+	const list = listTemplate.content.cloneNode(true).children[0];
+	const link = list.querySelector("[data-filter-link]");
+	link.textContent = element;
+	appliancesList.children[0].append(list);
+});
+// ustensils filter
+ustensilsArray.forEach((element) => {
+	const list = listTemplate.content.cloneNode(true).children[0];
+	const link = list.querySelector("[data-filter-link]");
+	link.textContent = element;
+	ustensilsList.children[0].append(list);
+});
 
 /** searchBar */
 const searchInput = document.getElementById("search");
 const message = document.querySelector("[data-message]");
+let count = 50; // number of recipes displayed
+let newCount = 0;
 searchInput.addEventListener("input", (e) => {
 	const value = e.target.value;
-	let messageVisible = false;
 	recipesArray.forEach((recipe) => {
 		if (value.length > 2) {
 			const isVisible =
 				recipe.title.toLowerCase().includes(value) ||
 				recipe.description.toLowerCase().includes(value);
 			recipe.element.classList.toggle("d-none", !isVisible);
-			message.classList.toggle("d-none", messageVisible);
 		} else {
 			recipe.element.classList.remove("d-none");
-			message.classList.add("d-none");
 		}
+		count = cardsWrapper.getElementsByClassName("d-none").length;
 	});
+	newCount = 50 - count;
+	const nbreRecipeString =
+		newCount === 0 || newCount === 1 ? " recette" : " recettes";
+	if (newCount === 0) {
+		message.innerHTML = `Aucune recette ne contient <span class="fw-bold">${e.target.value}</span> vous pouvez chercher «
+tarte aux pommes », « poisson », etc.... `;
+		message.classList.remove("d-none");
+	} else {
+		message.classList.add("d-none");
+	}
+	document.getElementById("recipesNumber").textContent =
+		newCount + nbreRecipeString;
 });
-
-/** init */
-//displayRecipes();
