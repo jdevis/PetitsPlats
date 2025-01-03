5b1fdbdb317e67193c9d26c760956dbe3fd87dc1
jdevis <josephine.roubertie@gmail.com>
2025-01-03 16:46:33 +0100

    working on listeners

--- js/main.js
+++ js/main.js
@@ -20,17 +20,9 @@ const searchInput = document.getElementById("searchInput");
 
 /** data manipulation and display */
 function filterArrays() {
-	console.log("tableau de recettes before: ", recipesArray);
 	let newRecipes = defaultRecipes;
-	console.log(
-		"tableau de tags: ",
-		tagsSelected,
-		"longueur: ",
-		tagsSelected.length
-	);
 	if (tagsSelected.length > 0) {
 		tagsSelected.forEach((tag) => {
-			console.log("tag: ", tag);
 			newRecipes = newRecipes.filter(
 				(recipe) =>
 					recipe.name.toLowerCase().includes(tag.toLowerCase()) ||
@@ -51,12 +43,13 @@ function filterArrays() {
 			);
 		});
 	}
-	console.log("tableau de recettes after: ", newRecipes);
 	return newRecipes;
 }
 
 function updateFilters() {
-	console.log("tableau ingrédients before: ", ingredientsArray);
+	ingredientsArray = [];
+	ustensilsArray = [];
+	appliancesArray = [];
 	recipesArray.forEach((recipe) => {
 		recipe.ingredients.forEach((ingredients) => {
 			ingredientsArray.push(ingredients.ingredient.toLowerCase());
@@ -75,7 +68,6 @@ function updateFilters() {
 	appliancesArray = [...new Set(appliancesArray)].sort((a, b) =>
 		a.localeCompare(b)
 	);
-	console.log("tableau ingrédients after: ", ingredientsArray);
 }
 
 function displayData(data, idWrapper, template) {
@@ -92,9 +84,13 @@ function displayData(data, idWrapper, template) {
 		idWrapper.appendChild(tag.createTag());
 	}
 	if (template == "filterList") {
+		let listItem = idWrapper.querySelectorAll("li");
+		listItem = listItem[0];
+		idWrapper.innerHTML = "";
+		idWrapper.appendChild(listItem);
 		data.forEach((filter) => {
 			const filterLi = new filterList(filter);
-			idWrapper.children[0].append(filterLi.filterTemplate());
+			idWrapper.appendChild(filterLi.filterTemplate());
 		});
 	}
 }
@@ -117,6 +113,7 @@ function removeElementFromArray(value, array) {
 }
 
 function countRecipes() {
+	//attention valeur de l'input prendre tag
 	let nb = cardsWrapper.querySelectorAll("article").length;
 	let searchValue = searchInput.value;
 	const nbreRecipeString = nb === 0 || nb === 1 ? " recette" : " recettes";
@@ -137,30 +134,35 @@ function listenToForms() {
 	allForms.forEach((form) => {
 		form.addEventListener("submit", (e) => {
 			e.preventDefault();
-			console.log("submit du form: ", form);
 			const inputValue = e.target.querySelector("input[type='search']")
 				.value;
 			const inputId = e.target.querySelector("input[type='search']").id;
-			console.log(
-				"valeur de l'input: ",
-				inputValue,
-				" longueur: ",
-				inputValue.length,
-				"id: ",
-				inputId
-			);
-			if (inputValue.length > 2) {
+			if (inputValue.length > 2 && !tagsSelected.includes(inputValue)) {
 				updateTags(inputValue);
-				recipesArray = filterArrays();
-				updateFilters();
 				displayData(inputValue, tagsWrapper, "tag");
-				displayData(recipesArray, cardsWrapper, "recipe");
-				displayData(ingredientsArray, ingredientsList, "filterList");
-				displayData(ustensilsArray, ustensilsList, "filterList");
-				displayData(appliancesArray, appliancesList, "filterList");
-				countRecipes();
 				removeTags();
+				init();
 			}
+		});
+	});
+}
+
+function listenToFiltersLists() {
+	const allLinks = document.querySelectorAll("ul.dropdown-menu li > a");
+	allLinks.forEach((link) => {
+		link.addEventListener("click", (e) => {
+			e.preventDefault();
+			console.log(
+				"click du lien: ",
+				e.target,
+				"avec valeur: ",
+				e.target.textContent
+			);
+			e.target.classList.add("selected");
+			updateTags(e.target.textContent);
+			displayData(e.target.textContent, tagsWrapper, "tag");
+			removeTags();
+			init();
 		});
 	});
 }
@@ -172,13 +174,7 @@ function removeTags() {
 			const value = e.target.parentNode.textContent;
 			e.target.parentNode.remove();
 			tagsSelected = removeElementFromArray(value, tagsSelected);
-			recipesArray = filterArrays();
-			updateFilters();
-			displayData(recipesArray, cardsWrapper, "recipe");
-			displayData(ingredientsArray, ingredientsList, "filterList");
-			displayData(ustensilsArray, ustensilsList, "filterList");
-			displayData(appliancesArray, appliancesList, "filterList");
-			countRecipes();
+			init();
 		});
 	});
 }
@@ -196,6 +192,7 @@ function init() {
 	displayData(appliancesArray, appliancesList, "filterList");
 	//mettre à jour le nbre de recettes et les afficher
 	countRecipes();
+	listenToForms();
+	listenToFiltersLists();
 }
 init();
-listenToForms();
